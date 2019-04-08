4e211454111003b0942c874bb148def2934584be
Christopher Dunn <cdunn@pacificbiosciences.com>
2019-04-07 18:02:50 -0700

    import *, for now

--- tests/t_kmers.nim
+++ tests/t_kmers.nim
@@ -1,21 +1,21 @@
 # vim: sw=4 ts=4 sts=4 tw=0 et:
 import unittest
-from pbpkg/kmers import hash
-from deques import items
-from sequtils import nil
-from sets import nil
+import pbpkg/kmers
+import deques
+import sequtils
+import sets
 
 test "bin_to_dna":
-    check kmers.bin_to_dna(0, 1, false) == "A"
-    check kmers.bin_to_dna(1, 1, false) == "C"
-    check kmers.bin_to_dna(2, 1, false) == "G"
-    check kmers.bin_to_dna(3, 1, false) == "T"
+    check bin_to_dna(0, 1, false) == "A"
+    check bin_to_dna(1, 1, false) == "C"
+    check bin_to_dna(2, 1, false) == "G"
+    check bin_to_dna(3, 1, false) == "T"
 
-    check kmers.bin_to_dna(0b00011011, 4, false) == "ACGT"
-    check kmers.bin_to_dna(0b00011011, 4, true)  == "TGCA"
+    check bin_to_dna(0b00011011, 4, false) == "ACGT"
+    check bin_to_dna(0b00011011, 4, true)  == "TGCA"
 
 test "dna_to_kmer":
-    check kmers.dna_to_kmer("AAAA", 2).seeds.len() == 6
+    check dna_to_kmer("AAAA", 2).seeds.len() == 6
 
 test "sorted_kmers":
     let
@@ -27,19 +27,19 @@ test "sorted_kmers":
             "TCGGCTACTATT",
         ]
         k = 12
-        kms: kmers.pot_t = kmers.dna_to_kmer(sq, k)
-    discard kmers.make_searchable(kms)  # sort
-    let got = sequtils.mapIt(kms.seeds, kmers.bin_to_dna(it.kmer, k.uint8, it.strand))
+        kms: pot_t = dna_to_kmer(sq, k)
+    discard make_searchable(kms)  # sort
+    let got = sequtils.mapIt(kms.seeds, bin_to_dna(it.kmer, k.uint8, it.strand))
     check got == expected
 
 test "search":
     let
         sq = "ATCGGCTACTATT"
         k = 12
-        kms: kmers.pot_t = kmers.dna_to_kmer(sq, k)
-        qms: kmers.pot_t = kmers.dna_to_kmer(sq, k)
-    check kmers.make_searchable(kms) == 0
-    let hits = kmers.search(kms, qms)
-    check deques.len(hits) == 4
-    check sets.len(sets.toHashSet(seqUtils.toSeq(hits))) == 4  # 4 unique items
+        kms: pot_t = dna_to_kmer(sq, k)
+        qms: pot_t = dna_to_kmer(sq, k)
+    check make_searchable(kms) == 0
+    let hits = search(kms, qms)
+    check hits.len() == 4
+    check sets.toHashSet(seqUtils.toSeq(hits)).len() == 4  # 4 unique items
     #check sets.len(sets.toHashSet(seqUtils.toSeq(deques.items(hits)))) == 4  # same as above

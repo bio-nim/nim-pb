de52d6ef1bdcbe745ac36d7d3309179ad8d2afcd
Christopher Dunn <cdunn@pacificbiosciences.com>
2019-04-16 11:39:27 -0700

    Drop most

--- src/pb.nim
+++ src/pb.nim
@@ -1,22 +1,7 @@
 # vim: sw=4 ts=4 sts=4 tw=0 et:
 import pbpkg/phasr
-
-proc dataset(extras: seq[string]) =
-    echo "pb dataset"
-proc kmers(int_dummy: int = 42, string_dummy: string = "hello") =
-    echo "pb kmers"
-proc utils(extras: seq[string], float_req: float) =
-    echo "pb utils ..."
 proc phasr() =
-    echo "starting"
     phasr.foo()
-    echo "finished"
-
 when isMainModule:
     import cligen
-    dispatchMulti(
-        [dataset, short = {}, help = {}],
-        [kmers, short = {"int_dummy": 'd'}, help = {}],
-        [utils, short = {}, help = {"float_req": "special help message"}],
-        [phasr],
-    )
+    dispatch(phasr)

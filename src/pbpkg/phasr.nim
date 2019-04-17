fb11e4f4b135b09345a9df74dedbf6dac9a3e25c
Christopher Dunn <cdunn@pacificbiosciences.com>
2019-04-17 09:50:20 -0700

    Iterate over groups of reads overlapping each given read, in sorted order

--- src/pbpkg/phasr.nim
+++ src/pbpkg/phasr.nim
@@ -1,24 +1,59 @@
 # vim: sw=4 ts=4 sts=4 tw=0 et:
+from sequtils import nil
 from strutils import format
+import deques
 import hts
 import os
 import ./kmers
 
 proc foo*() =
     echo "foo"
+proc showRec(record: Record) =
+    echo format("$# $# ($#) [$# .. $#] $#", record.tid, record.chrom, record.qname, record.start, record.stop,
+        ($record.cigar).substr(0, 32))
+
+iterator overlaps(b: hts.Bam): seq[Record] =
+    var current: Record
+    var current_stack_index: int
+    var stack = deques.initDeque[Record](64)
+    for r in b:
+        let record = hts.copy(r) # because iterator stores record on the Bam struct
+        stack.addLast(record)
+        if stack.len() == 1:
+            current_stack_index = 0
+            current = record
+        showRec(record)  # DEBUG
+        if record.start >= current.stop:
+            yield sequtils.toSeq(stack)  # YIELD
+
+            # Switch current to next record.
+            current_stack_index += 1
+            current = stack[current_stack_index]
+            for i in 0 .. current_stack_index - 2:
+                let ri = stack.peekFirst()
+                #echo " Pop?", ri.stop, "<=?", current.start
+                if ri.stop <= current.start:
+                    discard stack.popFirst()
+
+        #var rseq: string
+        #discard hts.sequence(record, rseq)
+        #var kmers: pot_t = dna_to_kmers(rseq, klen)
 
 proc readaln*(bfn: string; fasta: string) =
     const klen = 21
     var b: hts.Bam
 
     hts.open(b, bfn, index=true)
+    # We do not really need the index, but it proves that the Bam is sorted.
+    # Actually, we expect it to be sorted with @SO=coordinate, so maybe we
+    # should verify that. TODO(CD).
+    # Note that the sort is wrong for circular genomes; the secondary key is
+    # the query, not the coordinate. So we will need to do something tricky
+    # for circular references. TODO(CD).
     echo "[INFO] reading bam"
-    for record in b:
-        echo format("$# $# ($#) [$# .. $#] $#", record.tid, record.chrom, record.qname, record.start, record.stop,
-            ($record.cigar).substr(0, 32))
-        var rseq: string
-        discard hts.sequence(record, rseq)
-        var kmers: pot_t = dna_to_kmers(rseq, klen)
+    for ovlps in overlaps(b):
+        echo "len=", len(ovlps)
+        echo " range=", ovlps[0].start, "..", ovlps[^1].stop
  #[
 
   # for each overlapping read
